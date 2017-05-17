dddbc4c62d631e66cdf6bdf0cdce831d5c0df6be
Alessio Cammarata <alessio.cammarata@hotmail.com>
2017-05-17 15:26:08 +0200

    Ajout de commentaires

--- Utile/db.php
+++ Utile/db.php
@@ -1,8 +1,9 @@
+<!--Connexion à la base de données-->
 <?php
 
     $pdo = new PDO('mysql:host=localhost;dbname=projetphp', 'root', 'root');//connexion à la BDD
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Rapport d'erreurs
 
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
--- fonctions.php
+++ fonctions.php
@@ -5,12 +5,12 @@ function debug($variable){
     echo '<pre>' . print_r($variable, true) . '</pre>';
 
 }
-
+/*Création de clé utilisée pour un test*/
 function str_random($length){
     $alpha = "0123456789AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn";
     return substr(str_shuffle(str_repeat($alpha, $length)), 0, $length);
 }
-
+/*Fonction utilisée afin de vérifier si un utilisateur est connecté*/
 function logged_only(){
     if(session_status() == PHP_SESSION_NONE){
         session_start();
